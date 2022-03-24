73c6496b4804ddac19154029af6ac3b7366cede2
Luckysandwich7 <Ryanalvord@hotmail.com>
2022-03-23 20:29:04 -0600

    Initial File Upload

---
+++ final_project/rfk/game/casting/maze.py
@@ -0,0 +1,28 @@
+class Maze:
+    def __init__(self):
+       self.M = 10
+       self.N = 8
+       self.maze = [ 1,1,1,1,1,1,1,1,1,1,
+                     1,0,0,0,0,0,0,0,0,1,
+                     1,0,0,0,0,0,0,0,0,1,
+                     1,0,1,1,1,1,1,1,0,1,
+                     1,0,1,0,0,0,0,0,0,1,
+                     1,0,1,0,1,1,1,1,0,1,
+                     1,0,0,0,0,0,0,0,0,1,
+                     1,1,1,1,1,1,1,1,1,1,]
+                     #Set up maze with one string example: [ |, _,|, |,] and repeats the example X amount of times.
+
+    def draw(self,display_surf,image_surf):
+        bx = 0
+        by = 0
+        for i in range(0,self.M*self.N):
+            if self.maze[ bx + (by*self.M) ] == 1:
+                display_surf.blit(image_surf,( bx * 44 , by * 44))
+        
+            bx = bx + 1
+            if bx > self.M-1:
+                bx = 0 
+                by = by + 1
+
+
+                #Professor's Thoughts: "Have someone chase you in the maze"
